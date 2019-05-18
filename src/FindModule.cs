a6112e25440b0d3ff1e008579bbafe0c406f60a4
edwin young <edwin@bathysphere.org>
2019-05-17 17:16:17 -0700

    initial test querying powershell gallery

---
+++ src/FindModule.cs
@@ -0,0 +1,50 @@
+﻿using NuGet.Configuration;
+using NuGet.Frameworks;
+using NuGet.Packaging.Core;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+using NuGet.Common;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.PowerShell.PowerShellGet.NugetHelper
+{
+    public class FindModule
+    {
+        public async Task Find(string name, string version)
+        {
+            var packageVersion = NuGetVersion.Parse(version);
+            var packageIdentity = new PackageIdentity(name, packageVersion);
+
+            var nuGetFramework = NuGetFramework.ParseFolder("net46");
+            var settings = Settings.LoadDefaultSettings(root: null);
+
+            var packageSourceProvider = new PackageSourceProvider(
+                settings, new PackageSource[] { new PackageSource("https://www.powershellgallery.com/api/v2/") });
+
+            var sourceRepositoryProvider = new SourceRepositoryProvider(packageSourceProvider, Repository.Provider.GetCoreV3());
+
+            using (var cacheContext = new SourceCacheContext())
+            {
+                var repositories = sourceRepositoryProvider.GetRepositories();
+                var availablePackages = new HashSet<SourcePackageDependencyInfo>(PackageIdentityComparer.Default);
+
+                foreach (var sourceRepository in repositories)
+                {
+                    var dependencyInfoResource = await sourceRepository.GetResourceAsync<DependencyInfoResource>();
+                    var dependencyInfo = await dependencyInfoResource.ResolvePackage(
+                        packageIdentity, nuGetFramework, cacheContext, NullLogger.Instance , CancellationToken.None);
+
+                    if (dependencyInfo == null) continue;
+
+                    availablePackages.Add(dependencyInfo);
+                }
+            }
+        }
+    }
+}
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -6,10 +6,17 @@ namespace Microsoft.PowerShell.PowerShellGet.NugetHelper
 {
     public class UnitTest1
     {
-        [Fact]
+        //[Fact]
         public async Task Test1()
         {
             await Program.Main();
+        }
+
+        [Fact]
+        public async Task PSGallery_FindPowerShellGet()
+        {
+            FindModule f = new FindModule();
+            await f.Find("PowerShellGet","2.1.3");
         }
     }
 }
